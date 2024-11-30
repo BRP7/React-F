d56006f24544ec94a107f5d4f00d0dd839a7866f
BRP7 <bhumiparmar0007@gmail.com>
2024-11-30 11:43:21 +0530

    The Virtual & Real Dom Element.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client"
+import { createElement } from "react";
 
 function MainContent(){
   return(
@@ -6,8 +7,28 @@ function MainContent(){
   )
 }
 
+function createH1Element() {
+  const h1 = document.createElement("h1");
+  h1.className = "header";
+  h1.textContent = "Hey";
+  return h1;
+}
+
+
+let h1Element = createH1Element();
+// h1Element.textContent = "Hey";//cannot modify virtual elemet like this need status and components 
+// h1Element.classList.add = "header";
+document.body.appendChild(h1Element);  
+
 const root = createRoot(document.getElementById('root'));
 root.render(
+  <>
   <MainContent/>
+  <CreateVirtualH1Element/>
+  </>
 )
 
+function CreateVirtualH1Element() {
+  // Create the virtual React element
+  return createElement("h1", { className: "header" }, "Hey");
+}
